ad42f80c17cdeeff22ee32ee5859e3bdf1cf2a64
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-21 12:16:12 +0000

    autocommit

--- lectures/lecture_00_01/main.cpp
+++ lectures/lecture_00_01/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string nafn;
+    int aldur;
+
+    cout << "What's your name? ";
+    cin >> nafn;
+
+    cout << "How old are you? ";
+    cin >> aldur;
+
+    cout << "Hi " << nafn << endl;
+    cout << "Welcome to T208FOR2" << endl;
+
+    if (aldur >= 20) {
+        cout << "Congratulations, you can buy beer!" << endl;
+    }
     return 0;
 }
