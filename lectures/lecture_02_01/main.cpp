cb056d9cf5a104378315dd94da2dcbd6d3dd37c9
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-24 09:56:07 +0000

    autocommit

--- lectures/lecture_02_01/main.cpp
+++ lectures/lecture_02_01/main.cpp
@@ -4,12 +4,22 @@ using namespace std;
 
 int main()
 {
+    string nafn;
     int i = 0;
     while (i <= 10) {
-        cout << i << " - Hi!" << endl;
+        cout << "Please write a name: ";
+        cin >> nafn;
         i += 1;
+
+
     }
 
+    if () {
+        cout << "Elvis has arrived!" << endl;
+    }
+    else {
+        cout << "Nope, didn't see Elvis." << endl;
+    }
 
     return 0;
 }
