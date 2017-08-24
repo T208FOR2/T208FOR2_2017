d27659cc9ec70fa80f25b2a4deab0a33760637f3
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-24 07:21:09 +0000

    autocommit

--- lectures/lecture_01_01/main.cpp
+++ lectures/lecture_01_01/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
+    int tala;
+    cin >> tala;
 
+    if ( tala % 2 == 0 ) {
+        cout << "The number " << tala << " is even." << endl;
+    }
+    else {
+        cout << "The number " << tala << " is odd." << endl;
+    }
 
     return 0;
 }
