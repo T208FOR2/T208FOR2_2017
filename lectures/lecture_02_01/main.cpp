3e95bc54fbf4447436b9de3dbf058fbcb5da7bbe
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-24 09:41:47 +0000

    autocommit

--- lectures/lecture_02_01/main.cpp
+++ lectures/lecture_02_01/main.cpp
@@ -9,9 +9,11 @@ int main()
 
     if (a = b) {
         cout << "The numbers are the same!" << endl;
+        cout << a << " - " << b << endl;
     }
     else {
         cout << "Not the same numbers." << endl;
+        cout << a << " - " << b << endl;
     }
 
     return 0;
