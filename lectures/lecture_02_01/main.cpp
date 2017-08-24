6886dad56ce3e0e1bc8c33d129bcd562cc2bfefb
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-24 09:45:51 +0000

    autocommit

--- lectures/lecture_02_01/main.cpp
+++ lectures/lecture_02_01/main.cpp
@@ -7,9 +7,15 @@ int main()
     int a,b;
     cin >> a >> b;
 
-    if (a = b) {
+    if (a == b) {
         cout << "The numbers are the same!" << endl;
         cout << a << " - " << b << endl;
+    }
+    else if (a <= 0) {
+        cout << "a is very small" << endl;
+    }
+    else if (b >= 1000) {
+        cout << "b is pretty big" << endl;
     }
     else {
         cout << "Not the same numbers." << endl;
