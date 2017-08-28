b1eb7eeca2374f3a61687f2ad53c69fc245be6e4
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-28 14:36:13 +0000

    autocommit

--- lectures/lecture_03_01/main.cpp
+++ lectures/lecture_03_01/main.cpp
@@ -5,13 +5,16 @@ using namespace std;
 int main()
 {
     int staersta, naeststaersta, current;
+    bool fyrstaitrun = true;
 
     for (int i = 0; i < 10; i++) {
         cin >> current;
 
-        if (current > staersta) {
+        if (fyrstaitrun || current > staersta) {
             naeststaersta = staersta;
             staersta = current;
+
+            fyrstaitrun = false;
         }
         else if (current > naeststaersta) {
             naeststaersta = current;
