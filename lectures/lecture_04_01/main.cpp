a9284c9d99b135bab650d101e9c62c771d9c3ce5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-31 09:36:49 +0000

    autocommit

--- lectures/lecture_04_01/main.cpp
+++ lectures/lecture_04_01/main.cpp
@@ -56,20 +56,25 @@ int main()
         for (int row = 0; row < HEIGHT; row++) {
             for (int column = 0; column < WIDTH; column++) {
                 if (column == x && row == y) {
-                    if (d == UP) {
-                        cout << "^";
-                    }
-                    else if (d == RIGHT) {
-                        cout << ">";
-                    }
-                    else if (d == DOWN) {
-                        cout << "v";
-                    }
-                    else if (d == LEFT) {
-                        cout << "<";
+                    if (treasure_x == x && treasure_y == y) {
+
                     }
                     else {
-                        cout << "?";
+                        if (d == UP) {
+                            cout << "^";
+                        }
+                        else if (d == RIGHT) {
+                            cout << ">";
+                        }
+                        else if (d == DOWN) {
+                            cout << "v";
+                        }
+                        else if (d == LEFT) {
+                            cout << "<";
+                        }
+                        else {
+                            cout << "?";
+                        }
                     }
                 }
                 else {
