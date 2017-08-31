d403a2f4bae7de5cdcbc04912f6999aa527bf1f1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-31 08:51:52 +0000

    autocommit

--- lectures/lecture_04_01/main.cpp
+++ lectures/lecture_04_01/main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 const int HEIGHT = 10;
-const int WIDTH = 20;
+const int WIDTH = 20
 
 int main()
 {
+    int x = WIDTH / 2, y = HEIGHT / 2;
+
     for (int row = 0; row < HEIGHT; row++) {
         for (int column = 0; column < WIDTH; column++) {
             cout << ".";
