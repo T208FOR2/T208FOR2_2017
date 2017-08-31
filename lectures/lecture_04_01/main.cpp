9d8019d539255dbd25d68405ff7dcfb2154b1ec3
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-31 08:53:56 +0000

    autocommit

--- lectures/lecture_04_01/main.cpp
+++ lectures/lecture_04_01/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 const int HEIGHT = 10;
-const int WIDTH = 20
+const int WIDTH = 20;
 
 int main()
 {
@@ -11,7 +11,12 @@ int main()
 
     for (int row = 0; row < HEIGHT; row++) {
         for (int column = 0; column < WIDTH; column++) {
-            cout << ".";
+            if (column == x && row == y) {
+                cout << "O";
+            }
+            else {
+                cout << ".";
+            }
         }
         cout << endl;
     }
