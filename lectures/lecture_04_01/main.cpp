40070787d127d7df72b3502b0b8c87756227aebd
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-31 08:47:46 +0000

    autocommit

--- lectures/lecture_04_01/main.cpp
+++ lectures/lecture_04_01/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
+    for (int row = 0; row < 10; row++) {
+        for (int column = 0; column < 20; column++) {
+            cout << ".";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
