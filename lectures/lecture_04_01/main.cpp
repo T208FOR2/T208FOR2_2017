7c8522b02863df3be8e3ca776aaac7a1f224ad64
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-08-31 09:06:13 +0000

    autocommit

--- lectures/lecture_04_01/main.cpp
+++ lectures/lecture_04_01/main.cpp
@@ -10,31 +10,34 @@ enum directions{UP,RIGHT,DOWN,LEFT};
 int main()
 {
     int x = WIDTH / 2, y = HEIGHT / 2, d = LEFT;
+    char input = '.';
 
-    for (int row = 0; row < HEIGHT; row++) {
-        for (int column = 0; column < WIDTH; column++) {
-            if (column == x && row == y) {
-                if (d == UP) {
-                    cout << "^";
-                }
-                else if (d == RIGHT) {
-                    cout << ">";
-                }
-                else if (d == DOWN) {
-                    cout << "v";
-                }
-                else if (d == LEFT) {
-                    cout << "<";
+    while ( input != 'q' ) {
+        for (int row = 0; row < HEIGHT; row++) {
+            for (int column = 0; column < WIDTH; column++) {
+                if (column == x && row == y) {
+                    if (d == UP) {
+                        cout << "^";
+                    }
+                    else if (d == RIGHT) {
+                        cout << ">";
+                    }
+                    else if (d == DOWN) {
+                        cout << "v";
+                    }
+                    else if (d == LEFT) {
+                        cout << "<";
+                    }
+                    else {
+                        cout << "?";
+                    }
                 }
                 else {
-                    cout << "?";
+                    cout << ".";
                 }
             }
-            else {
-                cout << ".";
-            }
+            cout << endl;
         }
-        cout << endl;
     }
 
     return 0;
