5e75c61819375c55c2a8b28506c304043a10da3e
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 13:38:06 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -10,6 +10,9 @@ int main()
 
     cout << a << " / " << b << " = " << a / b << endl;
     cout << a << " / " << b << " = " << static_cast<double>(a) / b << endl;
+    cout << a << " / " << b << " = " << a / static_cast<double>(b) << endl;
+    cout << a << " / " << b << " = " << static_cast<double>(a) / static_cast<double>(b) << endl;
+    cout << a << " / " << b << " = " << static_cast<double>(a / b) << endl;
 
     return 0;
 }
