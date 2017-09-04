5bc9f46505e1aa32eb4bc7f2b908c790060a44f5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 13:25:51 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
+    int tala = 666;
+
+    cout << "The root of all evil is " << sqrt(tala) << endl;
 
     return 0;
 }
