fe04b1a0880e2f4bc6cd7227aebe32ee3dd624bd
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 13:46:13 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+
+
 int main()
 {
+    double x;
+    int n;
+    double nidurstada;
 
+    cin >> x >> n;
+
+    nidurstada = x_i_nta_veldi(x,n);
+
+    cout << x << " to the power of " << n << " is " << nidurstada << endl;
 
     return 0;
 }
