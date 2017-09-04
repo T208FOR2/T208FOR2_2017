9c3b9b9d818dbda539e727bbf08662deea00237f
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 14:36:37 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -2,31 +2,19 @@
 
 using namespace std;
 
-double x_i_nta_veldi(double& x, int& n);
-
+int testfall(int tala);
 
 int main()
 {
-    double tala;
-    int veldisvisir;
-    double nidurstada;
 
-    cin >> tala >> veldisvisir;
-
-    nidurstada = x_i_nta_veldi(tala, veldisvisir);
-
-    cout << tala << " to the power of " << veldisvisir << " is " << nidurstada << endl;
 
     return 0;
 }
 
-double x_i_nta_veldi(double& x, int& n) {
-    double result = x;
-    for (int i = 0; i < n-1; i++) {
-        result = result * x;
-    }
-    return result;
-}
 
+int testfall(int tala) {
+    tala = tala + 1;
+    return tala;
+}
 
 
