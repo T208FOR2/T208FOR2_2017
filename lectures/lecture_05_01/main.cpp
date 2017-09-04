55950860e415e538c4f475cac40827836ca6984a
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 14:38:40 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-int testfall(int tala);
+int testfall(int& tala);
 
 int main()
 {
+    int a;
+    cin >> a;
 
+    cout << a << endl;
+    cout << testfall(a) << endl;
+    cout << a << endl;
 
     return 0;
 }
 
 
-int testfall(int tala) {
+int testfall(int& tala) {
     tala = tala + 1;
     return tala;
 }
