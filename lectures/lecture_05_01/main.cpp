f959c827bc111467e5f30fc5fe282f51024024c1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-04 13:42:10 +0000

    autocommit

--- lectures/lecture_05_01/main.cpp
+++ lectures/lecture_05_01/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    int a,b;
-    cin >> a >> b;
 
-    cout << a << " / " << b << " = " << a / b << endl;
-    cout << a << " / " << b << " = " << static_cast<double>(a) / b << endl;
-    cout << a << " / " << b << " = " << a / static_cast<double>(b) << endl;
-    cout << a << " / " << b << " = " << static_cast<double>(a) / static_cast<double>(b) << endl;
-    cout << a << " / " << b << " = " << static_cast<double>(a / b) << endl;
 
     return 0;
 }
