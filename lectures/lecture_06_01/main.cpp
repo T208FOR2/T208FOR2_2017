ccdfc608c589dc7137655cf148ca1f6c0a4398b8
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-07 09:07:23 +0000

    autocommit

--- lectures/lecture_06_01/main.cpp
+++ lectures/lecture_06_01/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void sum(string s);
+string sum(string s);
 int sum(int a, int b);
 int sum(int a, int b, int c);
 
@@ -12,8 +13,14 @@ int main() {
 
     cout << a << " + " << b << " = " << sum(a,b) << endl;
     cout << a << " + " << b << " + " << c << " = " << sum(a,b,c) << endl;
+    cout << sum("something") << endl;
 
     return 0;
+}
+
+string sum(string s) {
+    cout << "i dont know how to sum \"" << s << "\"" << endl;
+    return "???";
 }
 
 int sum(int a, int b) {
