f80eb53cfd3816fdf1a0405cfe32d2bbc27600e1
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-07 08:40:50 +0000

    autocommit

--- lectures/lecture_06_01/main.cpp
+++ lectures/lecture_06_01/main.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
+
+
+
 int main()
 {
+    int d = 0;
+    char input = ' ';
+    while (input != 'q') {
+        if (input == 'r') {
+            d = (d + 1) % 4;
+        }
+
+    }
 
 
     return 0;
