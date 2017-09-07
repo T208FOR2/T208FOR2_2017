61dd53f19cf5388628f8152b042edeca74d609d2
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-07 09:03:16 +0000

    autocommit

--- lectures/lecture_06_01/main.cpp
+++ lectures/lecture_06_01/main.cpp
@@ -3,17 +3,22 @@
 using namespace std;
 
 int sum(int a, int b);
-
+int sum(int a, int b, int c);
 
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
 
     cout << a << " + " << b << " = " << sum(a,b) << endl;
+    cout << a << " + " << b << " + " << c << " = " << sum(a,b,c) << endl;
 
     return 0;
 }
 
 int sum(int a, int b) {
     return a + b;
+}
+
+int sum(int a, int b, int c) {
+    return a + b + c;
 }
