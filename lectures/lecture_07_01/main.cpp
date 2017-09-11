3c6d24f712c019c0f19f9aeaa1b18f1358e1f02c
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-11 13:50:52 +0000

    autocommit

--- lectures/lecture_07_01/main.cpp
+++ lectures/lecture_07_01/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+    ifstream theinputfile;
 
+    theinputfile.open("/Users/eyjo/Desktop/randomtolur.txt"); // MAC
+    theinputfile.open("c:\\Users\\eyjo\\Desktop\\randomtolur.txt");  // WINDOWS
 
     return 0;
 }
