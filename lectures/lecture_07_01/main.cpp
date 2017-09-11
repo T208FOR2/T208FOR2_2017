29de0d842e3f5b1ba1a164109e8a07141d38ff00
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-11 14:45:34 +0000

    autocommit

--- lectures/lecture_07_01/main.cpp
+++ lectures/lecture_07_01/main.cpp
@@ -4,14 +4,37 @@ using namespace std;
 
 void get_input(int& a, int& b);
 
+bool numbers_are_ok(int a, int b);
+
 int main()
 {
     int a,b;
     get_input(a,b);
 
+    if (numbers_are_ok(a,b)) {
+        cout << "Wheee!" << endl;
+    }
+    else {
+        cout << "Fuck!!!" << endl;
+    }
 
 
     return 0;
+}
+
+bool numbers_are_ok(int a, int b) {
+    if (b > a) {
+        return false;
+    }
+
+    while (b > 1) {
+        if (b % 10 != 0) {
+            return false;
+        }
+        b = b / 10;
+    }
+
+    return true;
 }
 
 void get_input(int& a, int& b) {
