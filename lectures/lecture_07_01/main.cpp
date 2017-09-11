41a1aba57749a26c723dff5c65fb0fff803114e7
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-11 13:52:56 +0000

    autocommit

--- lectures/lecture_07_01/main.cpp
+++ lectures/lecture_07_01/main.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main()
 {
     ifstream theinputfile;
+    int x;
 
     theinputfile.open("/Users/eyjo/Desktop/randomtolur.txt"); // MAC
-    theinputfile.open("c:\\Users\\eyjo\\Desktop\\randomtolur.txt");  // WINDOWS
+//    theinputfile.open("c:\\Users\\eyjo\\Desktop\\randomtolur.txt");  // WINDOWS
+
+    theinputfile >> x;
+
+    theinputfile.close();
+
+    cout << "The first number is: " << x << endl;
 
     return 0;
 }
