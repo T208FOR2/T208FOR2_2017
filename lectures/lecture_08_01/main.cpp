8460c45f943be04c33fad2ee02eecf0ba34021f8
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-21 09:55:09 +0000

    autocommit

--- lectures/lecture_08_01/main.cpp
+++ lectures/lecture_08_01/main.cpp
@@ -4,27 +4,18 @@ using namespace std;
 
 const int N = 4;
 
-int round_number(double x);
+
 
 int main()
 {
-    double numbers[N];
-    for (int i = 0; i < N; i++) {
-        numbers[i] = 0.0;
-    }
+    string texti[N];
 
     for (int i = 0; i < N; i++) {
-        cin >> numbers[i];
+        cin >> texti[i];
     }
-    cout << "---------------------" << endl;
-    for (int i = 0; i < N; i++) {
-        cout << round_number( numbers[i] ) << endl;
-    }
+
+
 
     return 0;
 }
 
-int round_number(double x) {
-    int round_result = (x+0.5);
-    return round_result;
-}
