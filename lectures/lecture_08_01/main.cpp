6a935b667ae45c152fb0fe293dbc501e62b709cc
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-21 09:32:37 +0000

    autocommit

--- lectures/lecture_08_01/main.cpp
+++ lectures/lecture_08_01/main.cpp
@@ -2,17 +2,18 @@
 
 using namespace std;
 
+const int N = 10;
+
 int main()
 {
-    int tolur[10];
+    int tolur[N];
 
-    for (int i = 0; i < 10; i++) {
-        cout << "Please write integer nr " << i+1 << ": " << endl;
+    for (int i = 0; i < N; i++) {
         cin >> tolur[i];
     }
     cout << "----------------" << endl;
 
-    for (int i = 9; i >= 0; i--) {
+    for (int i = N-1; i >= 0; i--) {
         cout << tolur[i] << endl;
     }
 
