51bd7086e22035740636aab2dfaf2c714992cb46
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-21 08:52:12 +0000

    autocommit

--- lectures/lecture_08_01/main.cpp
+++ lectures/lecture_08_01/main.cpp
@@ -5,22 +5,26 @@
 
 using namespace std;
 
+void open_my_file(ifstream& theinputfile, string thefilename );
+
 int main()
 {
     string thefilename;
     cin >> thefilename;
 
     ifstream theinputfile;
-    theinputfile.open( thefilename.c_str() );
-    if (theinputfile.fail()) {
-        cout << "Can't open the file: " << thefilename << endl;
-        exit(1);
-    }
-
-
+    open_my_file(theinputfile, thefilename);
 
     theinputfile.close();
     cout << "Thanks!" << endl;
 
     return 0;
+}
+
+void open_my_file(ifstream& theinputfile, string thefilename ) {
+    theinputfile.open( thefilename.c_str() );
+    if (theinputfile.fail()) {
+        cout << "Can't open the file: " << thefilename << endl;
+        exit(1);
+    }
 }
