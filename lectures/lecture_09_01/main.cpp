227515fce8694e62da70642380c8c8cfc6ab99fd
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-25 13:27:16 +0000

    autocommit

--- lectures/lecture_09_01/main.cpp
+++ lectures/lecture_09_01/main.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
+const int NUM = 10;
+
 int main() {
+    string texti[NUM], keyword = "";
+
+    for (int i = 0; i < NUM; i++) {
+        cin >> texti[i];
+    }
+
+    cout << "What word are you looking for? ";
+    cin >> keyword;
+    while (keyword != "quit") {
+
+        int index = -1;
+        for (int i = 0; i < NUM; i++) {
+            if (keyword == texti[i]) {
+                index = i;
+            }
+        }
+
+        if (index >= 0) {
+            cout << "The word" << keyword << " is number ";
+            cout << index << " in the list" << endl;
+        }
+        else {
+            cout << "Sorry, couldn't find the word " << keyword << endl;
+        }
+
+        cout << "What word are you looking for? ";
+        cin >> keyword;
+    }
 
 
     return 0;
