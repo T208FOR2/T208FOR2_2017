d777f2377c2e22e4e2c809822fe1bfe5a6b5fa0a
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-09-25 13:33:19 +0000

    autocommit

--- lectures/lecture_09_01/main.cpp
+++ lectures/lecture_09_01/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 const int NUM = 10;
 
+int find_element(string s[], int n, string keyword);
+
 int main() {
     string texti[NUM], keyword = "";
 
@@ -15,12 +17,7 @@ int main() {
     cin >> keyword;
     while (keyword != "quit") {
 
-        int index = -1;
-        for (int i = 0; i < NUM; i++) {
-            if (keyword == texti[i]) {
-                index = i;
-            }
-        }
+        int index = find_element(texti, NUM, keyword);
 
         if (index >= 0) {
             cout << "The word" << keyword << " is number ";
@@ -36,4 +33,14 @@ int main() {
 
 
     return 0;
+}
+
+int find_element(string s[], int n, string keyword) {
+    int index = -1;
+    for (int i = 0; i < n; i++) {
+        if (keyword == s[i]) {
+            index = i;
+        }
+    }
+    return index;
 }
