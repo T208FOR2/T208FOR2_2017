d8b811179e6b9914f2d4db7a5599b1001d9f74f8
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-05 08:42:18 +0000

    autocommit

--- lectures/lecture_11_01/main.cpp
+++ lectures/lecture_11_01/main.cpp
@@ -8,7 +8,8 @@ int main()
     string inputline = " VIDEO: Elements of the #DoD and @FEMA are providing humanitarian relief for #PuertoRico and #USVI.";
 
     pos = inputline.find("#");
-
+    cout << inputline << endl;
+    cout << pos << endl;
 
     return 0;
 }
