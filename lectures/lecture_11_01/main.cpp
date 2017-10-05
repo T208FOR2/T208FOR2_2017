1c2f22b57a534c2171bd8c80ff27f189516406aa
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-05 08:40:14 +0000

    autocommit

--- lectures/lecture_11_01/main.cpp
+++ lectures/lecture_11_01/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    unsigned int pos;
+    string inputline = " VIDEO: Elements of the #DoD and @FEMA are providing humanitarian relief for #PuertoRico and #USVI.";
+
+    pos = inputline.find("#");
+
 
     return 0;
 }
