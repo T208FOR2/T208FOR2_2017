b38bc702611880473c75845a5f73214591085059
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 13:40:29 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -2,26 +2,12 @@
 
 using namespace std;
 
+const int NUM = 100;
+
 int main()
 {
-    int n, current, themax;
-    int sum = 0;
-    bool firstiteration = true;
-
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cin >> current;
-        sum += current;
-
-        if (firstiteration || current > themax) {
-            themax = current;
-        }
-        firstiteration = false;
-    }
-
-    cout << "The average is " << static_cast<double>(sum)/n << endl;
-    cout << "The largest number is " << themax << endl;
+    int n;
+    int fylki[NUM];
 
     return 0;
 }
