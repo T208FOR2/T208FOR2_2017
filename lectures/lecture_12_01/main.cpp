9b5e6784e4b4d63b94881c0e6e962227ee41d0d2
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 14:06:47 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -1,33 +1,9 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-const int NUM = 100;
-
 int main()
 {
-    int n, sum = 0;
-    int fylki[NUM];
-    double sigma = 0, mean;
-
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cin >> fylki[i];
-        sum += fylki[i];
-    }
-    mean = static_cast<double>(sum)/n;
-
-    for (int i = 0; i < n; i++) {
-        //sigma += pow(fylki[i] - mean, 2);
-        sigma += (fylki[i] - mean)*(fylki[i] - mean);
-    }
-
-    sigma = sqrt( sigma / (n-1));
-
-    cout << "The average " << static_cast<double>(sum)/n << endl;
-    cout << "StdDev: " << sigma << endl;
 
     return 0;
 }
