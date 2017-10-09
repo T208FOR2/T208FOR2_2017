be45989f3e07f39cacdac6d3355aa09329d90f13
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 14:12:50 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    int shift;
+    string texti;
+
+    cin >> shift >> texti;
+
+    texti = texti.substr(1, texti.length());
+
+    cout << texti << endl;
 
     return 0;
 }
