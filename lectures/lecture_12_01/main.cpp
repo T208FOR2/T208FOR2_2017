98c957dc1457c338dfe638c9485e902336f3be76
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 14:45:38 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -13,7 +13,18 @@ int main()
     getline(cin, setning);
     setning = setning.substr(1,setning.length());
 
+    ???
 
+    if (word_appears) {
+        cout << "The word appears";
+        if (word_appears_multiple_times) {
+            cout << " multiple times"
+        }
+        cout << "." << endl;
+    }
+    else {
+        cout << "The word doesn't appear in the sentence." << endl;
+    }
 
 
     return 0;
