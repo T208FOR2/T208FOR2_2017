ccb39a908bf770aa8772271929cb2b692fb4f094
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 14:41:31 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -3,38 +3,15 @@
 
 using namespace std;
 
-unsigned int minvalue(unsigned int x, unsigned int y);
-
 int main()
 {
-    string a,b;
-    unsigned int shorter_length;
-    string result = "";
+    string ord, setning;
 
-    cin >> a >> b;
+    cin >> ord;
+    getline(cin, setning);
 
-    shorter_length = minvalue(a.length(), b.length());
-
-    for (unsigned int i = 0; i < shorter_length; i++) {
-        result = result + a[i] + b[i];
-    }
-
-    for (unsigned int i = shorter_length; i < a.length(); i++) {
-        result = result + a[i];
-    }
-    for (unsigned int i = shorter_length; i < b.length(); i++) {
-        result = result + b[i];
-    }
-
-    cout << result << endl;
+    cout << "\"" << setning << "\"" << endl;
 
     return 0;
-}
-
-unsigned int minvalue(unsigned int x, unsigned int y) {
-    if (x < y) {
-        return x;
-    }
-    return y;
 }
 
