7765c7cac32d233c4c47e0d37561d3769111fb9f
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 13:34:21 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -4,17 +4,24 @@ using namespace std;
 
 int main()
 {
-    int n, current;
+    int n, current, themax;
     int sum = 0;
+    bool firstiteration = true;
 
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         cin >> current;
         sum += current;
+
+        if (firstiteration || current > themax) {
+            themax = current;
+        }
+        firstiteration = false;
     }
 
     cout << "The average is " << static_cast<double>(sum)/n << endl;
+    cout << "The largest number is " << themax << endl;
 
     return 0;
 }
