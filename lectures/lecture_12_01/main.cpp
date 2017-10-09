f723e42e58524f92d3cc6701b91896dcae37291a
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 13:30:17 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -4,7 +4,17 @@ using namespace std;
 
 int main()
 {
+    int n, current;
+    int sum = 0;
 
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> current;
+        sum += current;
+    }
+
+    cout << "The average is " << static_cast<double>(sum)/n << endl;
 
     return 0;
 }
