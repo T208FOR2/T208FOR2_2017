23eef0673d8c26a6f3b6324aae1d6a3bfb5cdfbc
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-09 14:47:41 +0000

    autocommit

--- lectures/lecture_12_01/main.cpp
+++ lectures/lecture_12_01/main.cpp
@@ -13,7 +13,15 @@ int main()
     getline(cin, setning);
     setning = setning.substr(1,setning.length());
 
-    ???
+    pos = setning.find(ord);
+    if (pos < setning.length()) {
+        word_appears = true;
+        pos = setning.find(ord, pos+1);
+
+        if (pos < setning.length()) {
+            word_appears_multiple_times = true;
+        }
+    }
 
     if (word_appears) {
         cout << "The word appears";
