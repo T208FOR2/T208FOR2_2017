0a08a9f2d898495fcb99ac96fd0ead3ae45bc061
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-12 09:25:02 +0000

    autocommit

--- lectures/lecture_13_01/main.cpp
+++ lectures/lecture_13_01/main.cpp
@@ -4,7 +4,22 @@
 using namespace std;
 
 int main() {
+    int a[10];
+    int *p = a;
 
+    for (int i = 0; i < 10; i++) {
+        a[i] = i;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << a[i] << endl;
+    }
+
+    cout << "---------" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        cout << p[i] << endl;
+    }
 
 
     return 0;
