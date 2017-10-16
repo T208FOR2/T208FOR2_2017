c299dbce16999931cd635f5f7a6fa59415c6b1b2
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-16 13:41:55 +0000

    autocommit

--- lectures/lecture_14_01/main.cpp
+++ lectures/lecture_14_01/main.cpp
@@ -1,62 +1,23 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-void mirror_v1(string *fylki, int fjoldi);
-void mirror_v2(string* &fylki, int fjoldi);
-
-int main()
-{
-    string *s;
+int main() {
     int n;
+    int *p;
     cin >> n;
-
-    s = new string[n];
+    p = new int[n];
 
     for (int i = 0; i < n; i++) {
-        cin >> s[i];
+        cin >> p[i];
     }
 
-    // snua vid fylkinu!
-    mirror_v1(s, n);
-
     for (int i = 0; i < n; i++) {
-        cout << s[i] << " ";
+        cout << p[i] << " ";
     }
     cout << endl;
 
-    mirror_v2(s, n);
-
-    for (int i = 0; i < n; i++) {
-        cout << s[i] << " ";
-    }
-    cout << endl;
-
-
-    delete [] s;
+    delete [] p;
 
     return 0;
-}
-
-void mirror_v2(string* &fylki, int fjoldi) {
-    string *tmp_fylki = new string[fjoldi];
-
-    for (int i = 0; i < fjoldi; i++) {
-        tmp_fylki[fjoldi - i - 1] = fylki[i];
-    }
-
-    delete [] fylki;
-
-    fylki = tmp_fylki;
-}
-
-
-void mirror_v1(string *fylki, int fjoldi) {
-    string tmp;
-    for (int i = 0; i < fjoldi/2; i++) {
-        tmp = fylki[i];
-        fylki[i] = fylki[fjoldi-i-1];
-        fylki[fjoldi-i-1] = tmp;
-    }
 }
