11cdf545b15dd3124207e3526098603dcc2a17b0
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-16 14:36:32 +0000

    autocommit

--- lectures/lecture_14_01/main.cpp
+++ lectures/lecture_14_01/main.cpp
@@ -3,37 +3,8 @@
 using namespace std;
 
 int main() {
-    int rows, columns;
-    cin >> rows >> columns;
 
-    int **p = new int*[rows];
-    for (int i = 0; i < rows; i++) {
-        p[i] = new int[columns];
-    }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cin >> p[i][j];
-        }
-    }
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cout << p[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << "-------------------" << endl;
-    for (int i = 0; i < rows; i++) {
-        cout << p[i] << endl;
-    }
-    cout << "-------------------" << endl;
-
-    for (int i = 0; i < rows; i++) {
-        delete [] p[i];
-    }
-    delete [] p;
 
     return 0;
 }
