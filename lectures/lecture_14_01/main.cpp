f3fb0411c62d96b615b280dfdb88e1983ebfedd5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-16 15:25:15 +0000

    autocommit

--- lectures/lecture_14_01/main.cpp
+++ lectures/lecture_14_01/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 void transpose_2d_array(int **&a, int& rows, int& columns);
+int** create_2d_array(int& rows, int& columns);
+void delete_2d_array(int** a, int rows);
 
 int main() {
     int rows, columns;
@@ -30,26 +32,7 @@ int main() {
 
     cout << "-------------------" << endl;
 
-    int **p_bylt = new int*[columns];
-    for (int j = 0; j < columns; j++) {
-        p_bylt[j] = new int[rows];
-    }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            p_bylt[j][i] = p[i][j];
-        }
-    }
-
-    for (int i = 0; i < rows; i++) {
-        delete [] p[i];
-    }
-    delete [] p;
-
-    p = p_bylt;
-    int tmp = rows;
-    rows = columns;
-    columns = tmp;
 
 
     for (int i = 0; i < rows; i++) {
@@ -69,11 +52,12 @@ int main() {
     return 0;
 }
 
-void create_2d_array(int** &a, int& rows, int& columns) {
-    a = new int*[rows];
+int** create_2d_array(int& rows, int& columns) {
+    int** a = new int*[rows];
     for (int i = 0; i < rows; i++) {
         a[i] = new int[columns];
     }
+    return a;
 }
 
 void delete_2d_array(int **a, int rows) {
@@ -84,5 +68,19 @@ void delete_2d_array(int **a, int rows) {
 }
 
 void transpose_2d_array(int **&a, int& rows, int& columns) {
+    int **p_bylt = create_2d_array(columns, rows);
 
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            p_bylt[j][i] = a[i][j];
+        }
+    }
+
+    delete_2d_array(p, rows);
+
+    p = p_bylt;
+
+    int tmp = rows;
+    rows = columns;
+    columns = tmp;
 }
