1f388852c0e9d6b041215cc302ee6a17efcd635c
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-16 13:47:58 +0000

    autocommit

--- lectures/lecture_14_01/main.cpp
+++ lectures/lecture_14_01/main.cpp
@@ -3,20 +3,27 @@
 using namespace std;
 
 int main() {
-    int n;
-    int *p;
+    int n, m = 0;
+    int *p, *q;
     cin >> n;
     p = new int[n];
 
     for (int i = 0; i < n; i++) {
         cin >> p[i];
+        m += p[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        cout << p[i] << " ";
+    q = new int[m];
+
+
+
+
+    for (int i = 0; i < m; i++) {
+        cout << q[i] << " ";
     }
     cout << endl;
 
+    delete [] q;
     delete [] p;
 
     return 0;
