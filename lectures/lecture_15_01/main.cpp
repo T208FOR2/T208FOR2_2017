b0b7c596861ab9750f51cec0a957ed05a29ab81f
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 09:24:13 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+struct Namskeid {
+    string nafn;
+};
+
+struct Student {
+    string nafn;
+    int faedingarar;
+    string heimilisfang;
+
+    Namskeid uppahaldsfag;
+};
 
 
 int main()
