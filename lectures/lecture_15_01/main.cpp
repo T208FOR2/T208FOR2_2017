6ede1318d03aed184c4fb0b4923ddd5b817e1455
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 09:12:04 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -2,21 +2,11 @@
 
 using namespace std;
 
-struct Dagsetning {
-    int ar;
-    int manudur;
-    int dagur;
-    string vikudagur;
-};
 
 
 int main()
 {
     Dagsetning d;
-
-    cin >> d;
-    cout << d;
-
 
     return 0;
 }
