85ceaa303fe3697103d141853ac3159f818a08e5
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 08:47:43 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -9,11 +9,28 @@ struct Dagsetning {
     string vikudagur;
 };
 
+Dagsetning move_to_next_week(Dagsetning d);
+
+void prenta_dagsetning(Dagsetning d);
+
 int main()
 {
     Dagsetning d1;
 
+    d1.ar = 1999;
+    d1.manudur = 12;
+    d1.dagur = 31;
+    d1.vikudagur = "Sunnudagur";
 
 
     return 0;
+}
+
+Dagsetning move_to_next_week(Dagsetning d) {
+
+}
+
+void prenta_dagsetning(Dagsetning d) {
+    cout << d.dagur << "/" << d.manudur << "/" << d.ar <<;
+    cout << " (" << d.vikudagur << ")" << endl;
 }
