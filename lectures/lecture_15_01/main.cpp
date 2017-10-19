da3fd8ad6931a376c9415ad384304b3061141530
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 09:26:16 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -18,6 +18,15 @@ struct Student {
 
 int main()
 {
+    Namskeid n1, n2;
+    n1.nafn = "Forritun";
+    n2.nafn = "Staerdfraedi";
+
+    Student s1,s2,s3;
+    s1.nafn = "Jon";
+    s1.faedingarar = 1992;
+    s1.heimilisfang = "Laugavegur 11";
+    s1.uppahaldsfag = n1;
 
     return 0;
 }
