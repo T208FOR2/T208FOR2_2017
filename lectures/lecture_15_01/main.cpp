6ba91b6d8abe75a6bcd05e94389f719d67656c47
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 08:51:46 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -27,10 +27,21 @@ int main()
 }
 
 Dagsetning move_to_next_week(Dagsetning d) {
-
+    Dagsetning tmp = d;
+    // pretend all months have 30 days!
+    tmp.dagur += 7;
+    if (tmp.dagur > 30) {
+        tmp.dagur -= 30;
+        tmp.manudur++;
+    }
+    if (tmp.manudur > 12) {
+        tmp.manudur -= 12;
+        tmp.ar++;
+    }
+    return tmp;
 }
 
 void prenta_dagsetning(Dagsetning d) {
-    cout << d.dagur << "/" << d.manudur << "/" << d.ar <<;
+    cout << d.dagur << "/" << d.manudur << "/" << d.ar;
     cout << " (" << d.vikudagur << ")" << endl;
 }
