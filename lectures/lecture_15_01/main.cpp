ec8a7caa34bead599a347a07d9e05041878570c3
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-19 09:07:57 +0000

    autocommit

--- lectures/lecture_15_01/main.cpp
+++ lectures/lecture_15_01/main.cpp
@@ -9,46 +9,17 @@ struct Dagsetning {
     string vikudagur;
 };
 
-Dagsetning move_to_next_week(Dagsetning d);
-
-void prenta_dagsetning(Dagsetning d);
 
 int main()
 {
-    Dagsetning d1;
+    Dagsetning d;
 
-    d1.ar = 1999;
-    d1.manudur = 12;
-    d1.dagur = 24;
-    d1.vikudagur = "Sunnudagur";
+    cin >> d.ar >> d.manudur >> d.dagur >> d.vikudagur;
 
-    prenta_dagsetning(d1);
+    cout << d.dagur << "/" << d.manudur << "/" << d.ar << " (" << d.vikudagur << ")" << endl;
 
-    d1 = move_to_next_week(d1);
-
-    prenta_dagsetning(d1);
 
     return 0;
 }
 
 
-
-Dagsetning move_to_next_week(Dagsetning d) {
-    Dagsetning tmp = d;
-    // pretend all months have 30 days!
-    tmp.dagur += 7;
-    if (tmp.dagur > 30) {
-        tmp.dagur -= 30;
-        tmp.manudur++;
-    }
-    if (tmp.manudur > 12) {
-        tmp.manudur -= 12;
-        tmp.ar++;
-    }
-    return tmp;
-}
-
-void prenta_dagsetning(Dagsetning d) {
-    cout << d.dagur << "/" << d.manudur << "/" << d.ar;
-    cout << " (" << d.vikudagur << ")" << endl;
-}
