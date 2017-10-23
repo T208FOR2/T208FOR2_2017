89597b90d2249a379fd58b917ac3c7919fc7761f
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-23 13:22:31 +0000

    autocommit

--- lectures/lecture_16_01/main.cpp
+++ lectures/lecture_16_01/main.cpp
@@ -2,9 +2,39 @@
 
 using namespace std;
 
+class Timi {
+    public:
+        Timi();
+
+        void set_klst(int new_klst);
+        void set_minutur(int new_minutur);
+
+    private:
+        int klst;
+        int minutur;
+};
+
+
 int main()
 {
+    Timi t;
 
 
     return 0;
 }
+
+
+Timi::Timi() {
+    klst = 0;
+    minutur = 0;
+    cout << "Constructor: Timi()" << endl;  // Debug info
+}
+
+void set_klst(int new_klst) {
+    klst = new_klst;
+}
+
+void set_minutur(int new_minutur) {
+    minutur = new_minutur;
+}
+
