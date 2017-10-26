d2d46aaa51c32d1488b96980fe5bee7d4e536f91
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-26 08:50:59 +0000

    autocommit

--- lectures/lecture_17_01/main.cpp
+++ lectures/lecture_17_01/main.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class SantaBag{
+public:
+    SantaBag();
+    SantaBag(string nyr_eigandi, int nyr_hardir, int nyr_mjukir);
+
+private:
+    int hardirpakkar;
+    int mjukirpakkar;
+    string eigandi;
+
+};
+
+SantaBag::SantaBag() {
+    hardirpakkar = 0;
+    mjukirpakkar = 0;
+    eigandi = "";
+}
+
+SantaBag::SantaBag(string nyr_eigandi, int nyr_hardir, int nyr_mjukir) {
+    hardirpakkar = nyr_hardir;
+    mjukirpakkar = nyr_mjukir;
+    eigandi = nyr_eigandi;
+}
+
+
+
+
+
+
+
 int main() {
+    SantaBag poki1;
+
 
     return 0;
 }
