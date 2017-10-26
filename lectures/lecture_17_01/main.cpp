76abaeb15ee38c7c0d01a6c7f43ea097cd1d6a51
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-26 08:59:08 +0000

    autocommit

--- lectures/lecture_17_01/main.cpp
+++ lectures/lecture_17_01/main.cpp
@@ -8,6 +8,7 @@ public:
     SantaBag();
     SantaBag(string nyr_eigandi, int nyr_hardir, int nyr_mjukir);
 
+    friend ostream& operator << (ostream& outs, const SantaBag &poki);
 private:
     int hardirpakkar;
     int mjukirpakkar;
@@ -20,6 +21,13 @@ SantaBag::SantaBag() {
     mjukirpakkar = 0;
     eigandi = "";
     //cout << "Default constructor" << endl;
+}
+
+ostream& operator << (ostream& outs, const SantaBag &poki) {
+    outs << "Eigandi: " << poki.eigandi << ", hardir pakkar: ";
+    outs << poki.hardirpakkar << ", mjukir pakkar: " << poki.mjukirpakkar;
+
+    return outs;
 }
 
 SantaBag::SantaBag(string nyr_eigandi, int nyr_hardir, int nyr_mjukir) {
@@ -38,6 +46,9 @@ SantaBag::SantaBag(string nyr_eigandi, int nyr_hardir, int nyr_mjukir) {
 int main() {
     SantaBag poki1;
     SantaBag poki2("Stekkjastaur", 5, 7);
+
+    cout << poki1 << endl;
+    cout << poki2 << endl;
 
     return 0;
 }
