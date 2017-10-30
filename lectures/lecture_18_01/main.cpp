f1a025783c76f0f98c81d251098e4598c061a7e7
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-30 13:39:54 +0000

    autocommit

--- lectures/lecture_18_01/main.cpp
+++ lectures/lecture_18_01/main.cpp
@@ -6,6 +6,7 @@ class Numbers {
 public:
     Numbers();
 
+    void append(int i);
 private:
     int *p;
 
@@ -22,24 +23,40 @@ Numbers::Numbers() {
     max_elements = 0;
 }
 
+void Numbers::append(int i) {
+    if (n == max_elements) {
+        double_size_of_array();
+    }
+    p[n] = i;
+    n = n+1;
+}
+
 void Numbers::double_size_of_array() {
     cout << "****** double size ******" << endl;
     if (max_elements == 0) {
         max_elements = 2;
+        cout << "----------- NEW" << endl;
         p = new int[max_elements];
     }
     else {
         max_elements = max_elements * 2;
+        cout << "----------- NEW" << endl;
         int *new_p = new int[max_elements];
         for (int i = 0; i < n; i++) {
             new_p[i] = p[i];
         }
+        cout << "----------- DELETE" << endl;
         delete [] p;
         p = new_p;
     }
 }
 
 int main() {
+    Numbers num;
+
+    for (int i = 0; i < 10; i++) {
+        num.append(i);
+    }
 
     return 0;
 }
