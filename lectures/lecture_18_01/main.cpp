56738693c3647cfa447fdedbe9f4f7f94174401c
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-10-30 13:33:47 +0000

    autocommit

--- lectures/lecture_18_01/main.cpp
+++ lectures/lecture_18_01/main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
+class Numbers {
+public:
+    Numbers();
 
+private:
+    int *p;
+
+    int n;
+    int max_elements;
+};
+
+Numbers::Numbers() {
+    cout << "***** CONSTRUCTOR ******" << endl;
+    p = NULL;
+    n = 0;
+    max_elements = 0;
+}
 
 int main() {
 
