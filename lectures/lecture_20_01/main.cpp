eb4a255643bc9778039004667d571137b88f4d62
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-11-06 13:27:49 +0000

    autocommit

--- lectures/lecture_20_01/main.cpp
+++ lectures/lecture_20_01/main.cpp
@@ -2,9 +2,45 @@
 
 using namespace std;
 
+struct ListNode {
+    int data;
+    ListNode *next;
+};
+
+void print_reverse(ListNode *current) {
+    if (current == NULL) {
+        return;
+    }
+    else {
+        print_reverse(current->next);
+        cout << current->data << endl;
+    }
+}
+
 int main()
 {
+    ListNode *head = NULL, *tmp;
 
+    for (int i = 0; i < 10; i++) {
+        tmp = new ListNode;
+        tmp->data = i;
+        tmp->next = head;
+        head = tmp;
+    }
+
+    tmp = head;
+    while (tmp != NULL) {
+        cout << tmp->data << endl;
+        tmp = tmp->next;
+    }
+    cout << "-------------------" << endl;
+    print_reverse(head);
+
+    while (head != NULL) {
+        tmp = head->next;
+        delete head;
+        head = tmp;
+    }
 
     return 0;
 }
