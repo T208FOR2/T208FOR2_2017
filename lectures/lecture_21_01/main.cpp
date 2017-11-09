08d224a10788dba410b58f1e7784d998229fb9e3
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-11-09 09:39:58 +0000

    autocommit

--- lectures/lecture_21_01/main.cpp
+++ lectures/lecture_21_01/main.cpp
@@ -1,107 +1,31 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-
-class Team{
+class Dummy {
 public:
-    Team();
-    Team(string new_nafn, int new_attack, int new_defence, bool new_coach);
-
-    friend bool operator > (const Team& lhs, const Team& rhs);
-    friend Team operator + (const Team& lhs, const Team& rhs);
-
-    friend ostream& operator << (ostream& outs, const Team& t);
-    friend istream& operator >> (istream& ins, Team& t);
-
-private:
-    string nafn;
-    int attack;
-    int defence;
-    bool coach;
-
-    double strength() const;
+    Dummy(int k); ~Dummy();
+    int *p;
+    int n;
 };
 
-Team::Team() {
-    nafn = "";
-    attack = 0;
-    defence = 0;
-    coach = false;
+Dummy::Dummy(int k) {
+    p = new int[k];
+    n = k;
 }
 
-Team::Team(string new_nafn, int new_attack, int new_defence, bool new_coach) {
-    nafn = new_nafn;
-    attack = new_attack;
-    defence = new_defence;
-    coach = new_coach;
+Dummy::~Dummy() {
+    delete [] p;
 }
-
-bool operator > (const Team& lhs, const Team& rhs) {
-    return (lhs.strength() > rhs.strength());
-}
-
-Team operator + (const Team& lhs, const Team& rhs) {
-    Team newteam;
-
-    newteam.nafn = lhs.nafn + " - " + rhs.nafn;
-    if (lhs.attack > rhs.attack) {
-        newteam.attack = lhs.attack;
-    }
-    else {
-        newteam.attack = rhs.attack;
-    }
-
-    if (lhs.defence > rhs.defence) {
-        newteam.defence = lhs.defence;
-    }
-    else {
-        newteam.defence = rhs.defence;
-    }
-
-    newteam.coach = (lhs.coach || rhs.coach);
-
-    return newteam;
-}
-
-ostream& operator << (ostream& outs, const Team& t) {
-    outs << t.nafn << " (" << t.attack << ", " << t.defence << ")";
-    if (t.coach) {
-        outs << " with a coach";
-    }
-    return outs;
-}
-
-istream& operator >> (istream& ins, Team& t) {
-    int thecoach;
-    ins >> t.nafn >> t.attack >> t.defence >> thecoach;
-    if (thecoach == 1) {
-        t.coach = true;
-    }
-    else {
-        t.coach = false;
-    }
-    return ins;
-}
-
-double Team::strength() const {
-    if (coach) {
-        return (attack*attack + defence*defence)*1.2;
-    }
-    else {
-        return attack*attack + defence*defence;
+void testfunction(Dummy d) {
+    for (int i = 0; i < d.n; i++) {
+        cout << d.p[i] << endl;
     }
 }
-
 
 int main() {
-    Team Elite("The Elite Team", 90, 85, true);
-    Team A, B, C;
-    cin >> A >> B;
-    C = A + B;
-    cout << A << endl << B << endl << C << endl;
-    if (C > Elite) {
-        cout << C << " is better than the elite team!" << endl;
+    Dummy x(10);
+    for (int i = 0; i < 10; i++) {
+        cin >> x.p[i];
     }
+    testfunction(x);
     return 0;
 }
