32766cf4cfb3c500e2137fdfb0680bcde944fac4
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-11-09 09:29:44 +0000

    autocommit

--- lectures/lecture_21_01/main.cpp
+++ lectures/lecture_21_01/main.cpp
@@ -23,6 +23,41 @@ private:
     double strength();
 };
 
+Team::Team() {
+    nafn = "";
+    attack = 0;
+    defence = 0;
+    coach = false;
+}
+
+Team::Team(string new_nafn, int new_attack, int new_defence, bool new_coach) {
+    nafn = new_nafn;
+    attack = new_attack;
+    defence = new_defence;
+    coach = new_coach;
+}
+
+bool operator > (const Team& lhs, const Team& rhs) {
+}
+Team operator + (const Team& lhs, const Team& rhs) {
+}
+
+ostream& operator << (ostream& outs, const Team& t) {
+}
+
+istream& operator >> (istream& ins, Team& t) {
+
+}
+
+double Team::strength() {
+    if (coach) {
+        return (attack*attack + defence*defence)*1.2;
+    }
+    else {
+        return attack*attack + defence*defence;
+    }
+}
+
 
 int main() {
     Team Elite("The Elite Team", 90, 85, true);
