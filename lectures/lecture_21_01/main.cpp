866f5e9db6f183d58aeca5e152670ca0741eebbd
eyjolfur <eyjolfur.asgeirsson@gmail.com>
2017-11-09 08:36:54 +0000

    autocommit

--- lectures/lecture_21_01/main.cpp
+++ lectures/lecture_21_01/main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    string a,b,texti;
 
+    cin >> a >> b;
+    getline(cin, texti);
+
+    // go through texti and replace a with b
+    // strip whitespace and print out.
+
+    cout << "\"" << a << "\"" << endl;
+    cout << "\"" << b << "\"" << endl;
+    cout << "\"" << texti << "\"" << endl;
 
     return 0;
 }
